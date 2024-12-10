b724b7e27912dbe96c2621ce15da3a37fc58fc23
Chris Mellor <cmellor@gmail.com>
2024-12-10 17:05:54 +0700

    chore: port over AppServiceProvider.php code from v1

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,7 +2,9 @@
 
 namespace App\Providers;
 
+use Illuminate\Database\Eloquent\Model;
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Str;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -19,6 +21,15 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        Model::preventLazyLoading(! $this->app->isProduction());
+
+        // “Reading Time”
+        Str::macro('readingTime', function (...$text) {
+            $totalWords = str_word_count(implode(' ', $text));
+
+            $minutes = max(1, ceil(num: $totalWords / 200));
+
+            return ($minutes > 1) ? $minutes.' minutes' : $minutes.' minute';
+        });
     }
 }
