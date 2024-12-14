d3bc9181c4b8f3d40f81679d7f9622ca00bd4e13
Chris Mellor <cmellor@gmail.com>
2024-12-14 19:35:25 +0700

    refactor: replace Article controller/route with Folio page

--- routes/web.php
+++ routes/web.php
@@ -5,6 +5,4 @@ use Illuminate\Support\Facades\Route;
 
 Route::view('/', 'index')->name('homepage');
 
-Route::get('/articles', ArticleController::class)->name('articles');
-
 Route::view('/cv', 'cv')->name('cv');
