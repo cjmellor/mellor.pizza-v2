b18b67852f0aae4781300ae781f5c74b9883c7c0
Chris Mellor <cmellor@gmail.com>
2025-10-30 14:58:35 +0000

    style(floating-head): update background opacity and image object properties for improved aesthetics

--- resources/views/components/floating-head.blade.php
+++ resources/views/components/floating-head.blade.php
@@ -3,7 +3,7 @@
     <div class="bg-pizza dark:bg-pizza-dark absolute h-full w-full rounded-full blur-xl"></div>
     <div class="bg-pizza dark:bg-pizza-dark absolute h-full w-full animate-pulse rounded-full opacity-60 blur-2xl"></div>
     <div class="bg-pizza dark:bg-pizza-dark absolute -inset-0.5 rounded-full"></div>
-    <div class="absolute z-10 rounded-full">
+    <div class="absolute z-10 rounded-full bg-pizza/20 dark:bg-pizza-dark/20">
         <picture>
             <source
                 type="image/avif"
@@ -14,7 +14,7 @@
                 srcset="{{ asset('storage/faces/avatar-face.webp') }}"
             />
             <img
-                class="h-48 w-48 rounded-full grayscale lg:h-56 lg:w-56 dark:sepia"
+                class="h-48 w-48 rounded-full object-cover object-top grayscale lg:h-56 lg:w-56 dark:sepia"
                 src="{{ asset('storage/faces/avatar-face.png') }}"
                 alt="It's me!"
                 height="192"
